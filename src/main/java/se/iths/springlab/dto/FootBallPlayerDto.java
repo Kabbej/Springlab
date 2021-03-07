1c311cb912f990f97b67edd7dac1b4e03264f169
Kabbej <kabbe.jartun@gmail.com>
2021-03-07 13:32:27 +0100

    added first dto

---
+++ src/main/java/se/iths/springlab/dto/FootBallPlayerDto.java
@@ -0,0 +1,54 @@
+package se.iths.springlab.dto;
+
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+public class FootBallPlayerDto {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String team;
+
+
+    public FootBallPlayerDto(long id, String firstName, String lastName, String team) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.team = team;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+}
